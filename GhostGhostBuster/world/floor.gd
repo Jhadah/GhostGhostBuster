bde4281087608df2c8322e6365f9146fa3202f45
Jhadha <134785704+Jhadha@users.noreply.github.com>
2026-04-24 17:13:55 +0200

    sistemato gioco per fullscreen

--- GhostGhostBuster/world/floor.gd
+++ GhostGhostBuster/world/floor.gd
@@ -1,7 +1,4 @@
 extends Sprite2D
 
-func _ready() -> void:
-	region_rect.size = get_viewport_rect().size
-
-func _process(delta: float) -> void:
-	region_rect.position = Global.Player.position * 1.5
+func _process(_delta: float) -> void:
+	region_rect.position = Global.Player.position 
